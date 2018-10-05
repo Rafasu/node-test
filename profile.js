fec5972af0a52ea6ff9398dbd0ace29a4f4e77b2
Rafael Solis <rafasu8@gmail.com>
2018-10-05 10:22:45 -0600

    Added handling errors to profile.js

--- profile.js
+++ profile.js
@@ -3,6 +3,8 @@
 
 //Require https module
 const https = require('https');
+//Require http module
+const http = require('http');
 
 //Function to print message to console
 function printMessage(username, badgeCount, points) {
@@ -10,24 +12,43 @@ function printMessage(username, badgeCount, points) {
   console.log(message);
 }
 
-function getProfile(username) {
-  // Connect to the API URL (https://teamtreehouse.com/username.json)
-  const request = https.get(`https://teamtreehouse.com/${username}.json`, response => {
-                          let body = "";
-                          // Read the data
-                          response.on('data', data => {
-                            body += data.toString();
-                          });
+function printError(error){
+  console.error(error.message);
+}
 
-                          response.on('end', () => {
-                            // Parse the data
-                            const profile = JSON.parse(body);                            
-                            // Print the data
-                            printMessage(username, profile.badges.length, profile.points.JavaScript);
-                          });
-                          
-                          
-                      });
+function getProfile(username) {
+  try{
+  // Connect to the API URL (https://teamtreehouse.com/username.json)
+    const request = https.get(`https://teamtreehouse.com/${username}.json`, response => {
+      //Run code if response it's ok.
+      if(response.statusCode === 200){
+                            let body = "";
+                            // Read the data
+                            response.on('data', data => {
+                              body += data.toString();
+                            });
+
+                            response.on('end', () => {
+                              //Receiving another type of data.
+                              try{
+                                // Parse the data
+                                const profile = JSON.parse(body);                            
+                                // Print the data
+                                printMessage(username, profile.badges.length, profile.points.JavaScript);
+                                
+                              }catch(error){
+                                printError(error);
+                              } 
+                            });       
+      }else{
+        const message = `There was an error getting the profile for ${username} (${http.STATUS_CODES[response.statusCode]})` ;
+        const statusCodeError = new Error(message);
+        printError(statusCodeError);
+      }            
+    });
+  }catch(error){
+    printError(error);
+  }      
 
 }
 
